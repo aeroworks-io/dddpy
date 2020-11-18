3eb37f74fa1e10ac4f47200539ad4904694994d0
Yuichiro Smith <contact@yu-smith.com>
2020-11-18 23:09:16 +0900

    basic skeleton

---
+++ ddd/__init__.py
@@ -0,0 +1,6 @@
+__copyright__ = 'Copyright (C) 2020 Yuichiro Smith'
+__version__ = '0.0.1'
+__license__ = 'Apache License 2.0'
+__author__ = 'Yuichiro Smith'
+__author_email__ = 'contact@yu-smith.com'
+__url__ = 'http://github.com/aeroworks-io/python-ddd'
