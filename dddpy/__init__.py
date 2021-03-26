4e4fd598e4bd75881e03a1b1190c3dc3d6bdd40e
Yuichiro Smith <contact@yu-smith.com>
2021-03-27 00:09:28 +0900

    v0.4.1

--- dddpy/__init__.py
+++ dddpy/__init__.py
@@ -1,7 +1,7 @@
 from .bases import *
 
 __copyright__ = "Copyright (C) 2020 Yuichiro Smith"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __license__ = "Apache License 2.0"
 __author__ = "Yuichiro Smith"
 __author_email__ = "contact@yu-smith.com"
